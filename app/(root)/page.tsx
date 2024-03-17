5728af7c4051e30436423aba2e424f923f1bac11
Taz-war <fahimtazwer@gmail.com>
2024-03-18 04:05:23 +0600

    okkay before seheri

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Threads</h1>
-    </main>
-  );
+    <div className="h-screen">
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      <h1 className="head-text text-left">Home</h1>
+    </div>
+  )
 }
---
+++ components/shared/RIghtSideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RIghtSideBar = () => {
+  return (
+    <div>
+          RIghtSideBar
+    </div>
+  )
+}
+
+export default RIghtSideBar
